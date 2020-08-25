0446fa7743f00943a7aab27a55f30a6b6f63c123
Will Dunlap <dunlapww@gmail.com>
2020-08-24 18:43:41 -0600

    Initial commit

---
+++ test/ship_test.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+
+  def test_does_ship_exist
+    ship = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, ship
+  end
+
+  def test_does_ship_have_name_and_length
+    ship = Ship.new("Cruiser", 3)
+
+    assert_equal 3, ship.length
+    assert_equal "Cruiser", ship.name
+  end
+
+  def test_health_equal_length
+    ship = Ship.new("Cruiser", 3)
+
+    assert_equal 3, ship.length
+    assert_equal 3, ship.health
+    assert_equal true, ship.length == ship.health
+  end
+
+  def test_ship_sunk
+    ship = Ship.new("Cruiser", 3)
+
+    assert_equal false, ship.sunk?
+  end
+
+  def test_when_cruiser_is_hit_health_decremented_and_sunk
+    ship = Ship.new("Cruiser", 3)
+    ship.hit
+    assert_equal 2, ship.health
+    assert_equal false, ship.sunk?
+
+    ship.hit
+    assert_equal 1, ship.health
+    assert_equal false, ship.sunk?
+
+    ship.hit
+    assert_equal 0, ship.health
+    assert_equal true, ship.sunk?
+  end
+
+end
