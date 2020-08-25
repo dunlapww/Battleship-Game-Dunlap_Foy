ba0333e781bb42182e87f1b0f7850503c739fe29
Will Dunlap <dunlapww@gmail.com>
2020-08-24 21:58:55 -0600

    Add several cell test

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,32 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship, :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+    @fired_upon = false
   end
+
+  def empty?
+    @ship == nil ? true : false
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fire_upon
+    @fired_upon = true
+    @ship.health -= 1 if @ship.class == Ship && @ship.health > 0
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def render
+    "." if @fired_upon == false
+
+  end
+
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -16,10 +16,74 @@ class CellTest < Minitest::Test
     assert_equal "B4", cell.coordinate
   end
 
-  def test_ship_in_cell
+  def test_cell_initializes_wo_ship
     cell = Cell.new("B4")
 
     assert_nil cell.ship
   end
+
+  def test_cell_is_empty_if_no_ship
+    cell = Cell.new("B4")
+
+    assert_equal true, cell.empty?
+  end
+
+  def test_place_ship
+    ship = Ship.new("Cruiser", 3)
+    cell = Cell.new("B4")
+
+    cell.place_ship(ship)
+
+    assert_equal false, cell.empty?
+    assert_equal 'Cruiser', cell.ship.name
+  end
+
+  def test_when_cell_fired_upon_and_ship
+    ship = Ship.new("Cruiser", 3)
+    cell = Cell.new("B4")
+
+    cell.place_ship(ship)
+    cell.fire_upon
+    assert_equal 2, ship.health
+    cell.fire_upon
+    assert_equal 1, ship.health
+    cell.fire_upon
+    assert_equal 0, ship.health
+    cell.fire_upon
+    assert_equal 0, ship.health
+
+  end
+
+  def test_when_cell_fired_upon_an_no_ship
+    ship = Ship.new("Cruiser", 3)
+    cell = Cell.new("B4")
+
+    cell.fire_upon
+    assert_equal 3, ship.health
+  end
+
+  def test_cell_knows_its_fired_upon
+    cell = Cell.new("B4")
+    cell.fire_upon
+    # require "pry"; binding.pry
+    assert_equal true, cell.fired_upon?
+
+    cell = Cell.new("B4")
+    assert_equal false, cell.fired_upon?
+  end
+
+  def test_if_not_fired_upon_render_period
+    cell = Cell.new("B4")
+
+    assert_equal ".", cell.render
+
+    cell.fire_upon
+    refute_equal ".", cell.render
+
+  end
+
+
+
+
 
 end
