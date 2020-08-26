b326a9ad7e26775e286cd44fbbffe3540cf7954e
Will Dunlap <dunlapww@gmail.com>
2020-08-26 15:50:56 -0600

    Add coordinate method

--- test/board_test.rb
+++ test/board_test.rb
@@ -55,26 +55,19 @@ class BoardTest < Minitest::Test
     submarine = Ship.new("Submarine", 2)
     board = Board.new
 
+    # assert_equal true, board.valid_placement?(submarine, %w(A1 A2))
+    # assert_equal false, board.valid_placement?(submarine, %w(A1 A2 A3))
+    # assert_equal true, board.valid_placement?(submarine, %w(A1 A2))
     assert_equal false, board.valid_placement?(cruiser,%w(A1 A2))
-    assert_equal false, board.valid_placement?(submarine, %w(A1 A2 A3))
-    assert_equal true, board.valid_placement?(submarine, %w(A1 A2))
   end
+
 
   def test_ship_placement_is_consecutive
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     board = Board.new
 
-    board.valid_placement?(ship, ship_position)
-    ship_position = [a1, a2, a3]
-    (ship_position.first[1]..ship_position.last[1]).to_a = [1, 2, 3]
-ship_position = [a1, a2, a5]
-(ship_position.first[1]..ship_position.last[1]).to_a.size = 5
-ship_position.size = 3
-
-
-
-
+    board.valid_placement?(cruiser, ["A1", "B1", "C1"])
 
   end
 
